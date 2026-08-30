e116719e2c815f5dbd3de0ed09e27ed8bbf89dd1
Swagath <swagathreddy08@gmail.com>
2026-08-30 11:48:41 +0530

    profile_calculator.py and test3.py

---
+++ day-02/test3.py
@@ -0,0 +1,11 @@
+age_text = "28"
+age_number = int(age_text)
+print(age_number + 2)
+print(type(age_number))
+price = "99.5"
+print(float(price))
+print(str(100))
+a="Hello123"
+int(int(a))
+b=10.62
+print(int(b))
